9456c803b89de2ebc946735bcb342d415b48ccdb
KubaWasTaken <kuba.vopenka@gmail.com>
2024-08-09 15:37:43 +0200

    Add files via upload
    car :)

---
+++ javascript.js
@@ -0,0 +1,20 @@
+//function uvitej() {
+    //console.log("Hello welcome")
+   //console.log("Another greeting")
+//}
+
+//uvitej()
+
+//num1 = (2)
+//num2 = (3)
+
+//function spocitej(){
+    //console.log(num1 + num2)
+
+//}
+//spocitej()
+
+function vytiskni(){
+    var string ="hello";
+}
+vytiskni()
